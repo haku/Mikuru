0e7f20fb2ae3a29e437ec37dca85273330328ca2
はく <haku@vaguehope.com>
2011-09-16 14:21:47 +0100

    Move and clean script to get rsync binary.

--- com.vaguehope.mikuru/fetchbin.sh
+++ com.vaguehope.mikuru/fetchbin.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 wget http://adqmisc.googlecode.com/svn/trunk/androidutils/rsync/rsync-3.0.6-arm-softfloat-linux-gnueabi.gz
-zcat rsync-3.0.6-arm-softfloat-linux-gnueabi.gz > rsync
+zcat rsync-3.0.6-arm-softfloat-linux-gnueabi.gz > assets/rsync
+rm rsync-3.0.6-arm-softfloat-linux-gnueabi.gz
