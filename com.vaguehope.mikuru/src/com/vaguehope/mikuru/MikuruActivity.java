aa93305d30f4860085c05fc8c1df9af165742fb7
はく <haku@vaguehope.com>
2011-09-16 14:42:33 +0100

    Move RsyncTask to own class and tidy passing of args.

--- com.vaguehope.mikuru/src/com/vaguehope/mikuru/MikuruActivity.java
+++ com.vaguehope.mikuru/src/com/vaguehope/mikuru/MikuruActivity.java
@@ -1,18 +1,11 @@
 package com.vaguehope.mikuru;
 
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.vaguehope.mikuru.ExecHelper.LineProcessor;
 
 public class MikuruActivity extends Activity {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -41,8 +34,8 @@ public class MikuruActivity extends Activity {
 	private OnClickListener btnRunSync_onClick = new OnClickListener() {
 		@Override
 		public void onClick (View v) {
-			RsyncRun task = new RsyncRun();
-			task.execute();
+			RsyncTask task = new RsyncTask(MikuruActivity.this, MikuruActivity.this.txtConsole);
+			task.execute("--help");
 		}
 	};
 	
@@ -52,44 +45,6 @@ public class MikuruActivity extends Activity {
 			MikuruActivity.this.txtConsole.setText("");
 		}
 	};
-	
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	protected class RsyncRun extends AsyncTask<Void, String, Void> {
-		
-		@Override
-		protected Void doInBackground (Void... params) {
-			try {
-				RsyncHelper.readyRsync(MikuruActivity.this);
-				
-				List<String> args = new LinkedList<String>();
-				args.add("--help");
-				
-				ProcessBuilder pb = RsyncHelper.makeProcessBulder(MikuruActivity.this, args );
-				ExecHelper.expectExec(pb, this.lineProc);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			return null;
-		}
-		
-		protected final LineProcessor lineProc = new LineProcessor() {
-			@Override
-			@SuppressWarnings("synthetic-access")
-			public boolean processLine (String line) {
-				publishProgress(new String[] { line });
-				return true;
-			}
-		};
-		
-		@Override
-		protected void onProgressUpdate (String... values) {
-			if (values == null || values.length < 1) return;
-			MikuruActivity.this.txtConsole.append(values[0]);
-			MikuruActivity.this.txtConsole.append("\n");
-		}
-		
-	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
---
+++ com.vaguehope.mikuru/src/com/vaguehope/mikuru/RsyncTask.java
@@ -0,0 +1,57 @@
+package com.vaguehope.mikuru;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import com.vaguehope.mikuru.ExecHelper.LineProcessor;
+
+public class RsyncTask extends AsyncTask<String, String, Void> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private final Context context;
+	private final TextView txtConsole;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	public RsyncTask (Context context, TextView txtConsole) {
+		this.context = context;
+		this.txtConsole = txtConsole;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	protected Void doInBackground (String... params) {
+		try {
+			RsyncHelper.readyRsync(this.context);
+			ProcessBuilder pb = RsyncHelper.makeProcessBulder(this.context, Arrays.asList(params));
+			ExecHelper.expectExec(pb, this.lineProc);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return null;
+	}
+	
+	protected final LineProcessor lineProc = new LineProcessor() {
+		@Override
+		@SuppressWarnings("synthetic-access")
+		public boolean processLine (String line) {
+			publishProgress(new String[] { line });
+			return true;
+		}
+	};
+	
+	@Override
+	protected void onProgressUpdate (String... values) {
+		if (values == null || values.length < 1) return;
+		this.txtConsole.append(values[0]);
+		this.txtConsole.append("\n");
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
